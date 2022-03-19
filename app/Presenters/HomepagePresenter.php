3eab260c8b2b3cc4e2a1739ad855636204d5373d
jirka <jorgetimes2@gmail.com>
2022-03-19 10:18:05 +0100

    Nette\Cachinh\Cache first part

--- app/Presenters/HomepagePresenter.php
+++ app/Presenters/HomepagePresenter.php
@@ -6,17 +6,30 @@ namespace App\Presenters;
 
 use App\Model\PostManager;
 use Nette\Application\UI\Presenter;
+use Nette\Caching\Cache;
+use Nette\Caching\Storage;
 
 
 final class HomepagePresenter extends Presenter
 {
-    public function __construct(private PostManager $postManager)
-    {
+    private Cache $cache;
 
+    public function __construct(
+        private PostManager $postManager,
+        private Storage $storage
+    ) {
+        $this->cache = new Cache($storage, 'HomepagePresenter');
     }
 
     public function renderDefault()
     {
+        $value = $this->cache->load('nazdar', function () {
+            bdump('inside anonymous function');
+            return 'čau';
+        });
+
+        bdump($value);
+
         $this->template->posts = $this->postManager->getPublicPosts(5);
     }
 }
